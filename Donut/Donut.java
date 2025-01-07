3cc61e42e9c711df3755e86594c808427b83cd84
Josh Hodges <sick_libra@hotmail.com>
2025-01-06 20:41:32 -0500

    update readme begin random number verification

--- Donut/Donut.java
+++ Donut/Donut.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Donut{
+    //declare open var for secret number. 
+    public static int secret;
+
+    //This function is the prompt that utilizes a while loop to ensure the correct input for the menu choice.
     public static boolean getRules(){
         System.out.println("This is a guess the number game./n would you like to hear the rules? Y/N");
         Scanner scnr = new Scanner(System.in);
-        String rules="Z";
+        String rules="Z";//set rules to an arbitrary character to kick off while loop
         while (!rules.toUpperCase().equals("Y") || !rules.toUpperCase().equals("N") ) {
             rules= scnr.next();
-            rules.toUpperCase();
-            System.out.println(rules + "--");
+            rules= rules.toUpperCase();//equates to ignore case
             if(rules.equals("Y")){
                 scnr.close();
                 return true;
@@ -17,17 +21,36 @@ public class Donut{
         scnr.close();
         return false;
     }
+    //self explanitory prints rules
     public static void printRules(){
-        System.out.println("The computer will pick a random 3 digit number. Your job is to guess" +/n + "that number in 10 guesses or less. When you make a guess you will get feedback on your guess in the following ways:");
+        System.out.println("The computer will pick a random 3 digit number. Your job is to guess that number in 10 guesses or less.");
+        System.out.println( "When you make a guess you will get feedback on your guess in the following ways:");
         System.out.println("right number wrong place");
         System.out.println("All of your numbers are wrong");
         System.out.println("one of your numbers is right");
         System.out.println("Winner Winner");
     }
-    public static void main(String args[]){
-        Boolean rules = getRules();
-        if (rules == true){
-            printRules();
+    public static void generateRando(){
+        Random rand = new Random();
+        int temp = rand.nextInt(100,999);
+        boolean valid = false;
+        while (!valid){
+            // valid=checknum(temp);
         }
+    }
+    // public static boolean checknum(int num){
+        
+    // }
+    public static void main(String args[]){
+        Boolean rules = getRules();//starts with the menu prompt to see if player wants to see the rules.
+        if (rules == true){
+            printRules();//if they want them they get them 
+        }
+        boolean playing = true;//this allows player to play as many times as theyd like.
+        while (playing){
+            generateRando();
+        }
+
+
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 This collection of small programming projects contains a mixture of different languages to accomplish the 
 small projects in the big book of small python projects. 
+As a series of exercises for my own educational purposes, many of the projects will include overly complicated code. 
+I am fully aware that there are many much more efficient ways of conducting these exercises. 
+The reasons for overcomplication will be commented within the projects many things will simply be for repetition.
+The end goal is to lump all of these programs together in a menu like interface that will connect all of the 
+different games and hold the player as an object that will contain all of the high scores and number of 
+times each game has been played and serialize the information in some form. 
+The leading idea is to serialize the players in a set that will utilize xstream's marshalling for clarity
+in a csv file.
