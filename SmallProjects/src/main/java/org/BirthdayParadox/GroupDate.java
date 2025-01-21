6b6375ec54dd1eb76d86b7fae4893d900a4e44b8
Josh Hodges <sick_libra@hotmail.com>
2025-01-21 12:02:05 -0500

    delare tuple and sync dependency

--- SmallProjects/src/main/java/org/BirthdayParadox/GroupDate.java
+++ SmallProjects/src/main/java/org/BirthdayParadox/GroupDate.java
@@ -1,7 +1,17 @@
 package org.BirthdayParadox;
 
+import org.javatuples.Pair;
+/*in this I abandoned the idea of using the Date time functions using the
+* epoch time because it is more beneficial at my current position to learn
+* to use tuples. By using tuples I am effectively teaching myself how to
+* use dependencies in a project. */
 public class GroupDate {
 
     //Dependency for tuple in build.gradle.kts need to figure out how to use.
-    Pair<String, Integer> group;
+    final [] months =
+    Pair<String, Integer> date;
+    Pair[] groupDates;
+    public GroupDate(int qty) {
+        for (int i = 0; i < qty; i++) {}
+    }
 }
